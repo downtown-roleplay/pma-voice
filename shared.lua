59772da5b06434fd98736c822e17f52a99ca1e0d
sorambr <caecronos@hotmail.com>
2025-01-20 11:49:01 -0300

    feat(pma-voice): update voice mode distances and labels

--- shared.lua
+++ shared.lua
@@ -45,18 +45,18 @@ Player = Player
 Entity = Entity
 
 if GetConvar('voice_useNativeAudio', 'false') == 'true' then
-    -- native audio distance seems to be larger then regular gta units
-    Cfg.voiceModes = {
-        { 1.5, "Susurrando" }, -- Whisper speech distance in gta distance units
-        { 3.0, "Normal" }, -- Normal speech distance in gta distance units
-        { 7.0, "Gritando" } -- Shout speech distance in gta distance units
-    }
+	-- native audio distance seems to be larger then regular gta units
+	Cfg.voiceModes = {
+		{ 1.5, "Whisper" }, -- Whisper speech distance in gta distance units
+		{ 3.0, "Normal" },  -- Normal speech distance in gta distance units
+		{ 6.0, "Shouting" } -- Shout speech distance in gta distance units
+	}
 else
 	Cfg.voiceModes = {
-		{ 3.5,  "Susurrando" }, -- Whisper speech distance in gta distance units
-        { 15.0,  "Normal" },  -- Normal speech distance in gta distance units
-		{ 30.0, "Gritando"}, -- Shout speech distance in gta distance units
-		{ 50.0, "Gritando Alto"} -- Shout speech distance in gta distance units
+		{ 3.0,  "Whisper" }, -- Whisper speech distance in gta distance units
+		{ 7.0,  "Normal" },  -- Normal speech distance in gta distance units
+		{ 15.0, "Shouting" }, -- Shout speech distance in gta distance units
+		{ 30.0, "Shouting 2" } -- Shout speech distance in gta distance units
 	}
 end
 
